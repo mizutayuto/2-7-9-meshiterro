69b0cd1bb0b8949793f193b9bfcf20c96216619a
mizutayuto <mizuta.yuto.infratop@gmail.com>
2021-09-24 18:42:20 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0fc0b63e88f24701b368a6c56baf40bdfb9cc58d4854326359db51681bceb602cb8f1b0e6cb97a01218256b2c44b8a30d9cfce4283e637c287bdd9e1959f286f'
