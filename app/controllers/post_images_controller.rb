1bb0779e646612bb099e574d24ec7bf39915702c
mizutayuto <mizuta.yuto.infratop@gmail.com>
2021-09-25 12:38:00 +0000

    [Finish]4-5

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -12,11 +12,12 @@ class PostImagesController < ApplicationController
   end
   
   def index
-    @post_images = PostImage.all
+     @post_images = PostImage.page(params[:page]).reverse_order
   end
   
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
   
   def destroy
@@ -24,6 +25,7 @@ class PostImagesController < ApplicationController
     @post_image.destroy
     redirect_to post_images_path
   end
+
   
   private
   
